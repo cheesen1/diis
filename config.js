6c9d423fcd6d5ce12b5d9f67c067dedb9c99a36b
cheesen1 <92022620+cheesen1@users.noreply.github.com>
2023-04-29 06:11:32 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  token: "NjQ5MTY1NDUyNDA4Mzg5NjM3.G1xCJ3.P3nuSifFuXEGjdJR_QJVlB2o5F9sxEpomW39kk",
+
+  status: 'ปั๊ปปี้ก็จะแบบ นี่มันกูชัดๆ',
+  applicationId: '1101032397648166982',
+  url: 'https://www.youtube.com/watch?v=F4Sf0OYfNIA&ab_channel=qlcvea',
+  state: 'Cheeseno',
+  name: 'Cheeseno',
+  details: 'LIVE',
+
+  largeImage: 'https://cdn.discordapp.com/attachments/969615393679372308/1101592133334474782/touhou-fumo.gif',
+  largeText: '🐶🤍',
+
+  smallImage: 'https://cdn.discordapp.com/attachments/969615393679372308/1101594260438003762/cirno-touhou.gif',
+  smallText: 'cirno',
+
+  button1: {
+    name: 'ตลกหรอคับ',
+    value: 'https://www.facebook.com/profile.php?id=100088118030644'
+  },
+  button2: {
+    name: 'ตลกมาก',
+    value: 'https://cdn.discordapp.com/attachments/969615393679372308/1101595599679279105/frog-flip.gif'
+  }
+}
